71f88fe476e5e0abf799135a7ad2fb243e9b846c
anggeryb <angger@entrepreneurs.id>
2021-04-15 15:29:08 +0700

    setup midtrans

---
+++ app/Http/Controllers/MidtransController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\MidtransPayment;
+
+class MidtransController extends Controller
+{
+    public function form(){
+        return view('Midtrans.form');
+    }
+
+    public function midtransSubmit(Request $request){
+        $name = $request->input('name');
+        $email = $request->input('email');
+        
+        $phone = $request->input('phone');
+        $exId = uniqid();
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://app.sandbox.midtrans.com/snap/v1/transactions',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => false,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 
+        '{
+            "transaction_details": {
+                "order_id": "' . $exId . '",
+                "gross_amount": 250000
+            },
+            "credit_card":{
+                "secure" : true
+            },
+            "customer_details": {
+                "first_name": "' . $name . '",
+                "email": "' . $email . '",
+                "phone": "' . $phone . '"
+            }
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: Basic U0ItTWlkLXNlcnZlci1hTXpkLXpxTXlYVHh6QTR2LW5MMFNzWkE6'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $response = json_decode($response, TRUE);
+        $token = $response['token'];
+        curl_close($curl);
+        return view('Midtrans.form2', compact('token'));
+
+    }
+}
---
+++ resources/views/Midtrans/form2.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript"
+      src="https://app.sandbox.midtrans.com/snap/snap.js"
+      data-client-key="SB-Mid-client-RU8wIZ5asOLAJEKv"></script>
+    <title>Midtrans Payment</title>
+</head>
+<body>
+    <button id="pay-button">Pay!</button>
+    <script type="text/javascript">
+      var payButton = document.getElementById('pay-button');
+      // For example trigger on button clicked, or any time you need
+      payButton.addEventListener('click', function () {
+        snap.pay('{{ $token }}'); // Replace it with your transaction token
+      });
+    </script>
+  </body>
+</html>
