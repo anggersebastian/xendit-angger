60f9889bcae663e1d0aadf801bbf83f266d58280
anggeryb <angger@entrepreneurs.id>
2021-04-23 11:02:29 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/thanks', 'HomeController@thankYou')->name('thankYou');
 
 // Xendit 
 Route::get('/xendit/payment', 'PaymentController@createInvoice');
